6560cf12743d68ee1dd8c7db204ca02ecaaab4d1
akkuldn <akkul.dn@qxf2.com>
2020-08-06 18:59:35 +0530

    Added Remote options as a seperate module

--- page_objects/Remote_Options.py
+++ page_objects/Remote_Options.py
@@ -1,7 +1,12 @@
+"""
+Set the desired capabilities while running the test on a remote platform
+"""
+
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from datetime import datetime
 
 class Remote_Options():
+    "Class contains methods for various remote options for browserstack and saucelab"
 
     def firefox(browser_version):
         "Set web browser as firefox"
@@ -52,11 +57,20 @@ class Remote_Options():
         desired_capabilities['os_version'] = os_version
         return desired_capabilities
 
-
     def remote_project_name(desired_capabilities, remote_project_name):
+        "Set remote project name for browserstack"
+
         desired_capabilities['project'] = remote_project_name
         return desired_capabilities
 
     def remote_build_name(desired_capabilities, remote_build_name):
+        "Set remote build name for browserstack"
+
         desired_capabilities['build'] = remote_build_name+"_"+str(datetime.now().strftime("%c"))
+        return desired_capabilities
+
+    def saucelab_platform(desired_capabilities, os_name, os_version):
+        "Set platform for saucelab"
+
+        desired_capabilities['platform']= os_name + ' '+os_version
         return desired_capabilities
